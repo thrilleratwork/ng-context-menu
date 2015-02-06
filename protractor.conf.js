2af8e3df785ab069ce4275a6f2efdb8d0917291d
Ian Walter <iankwalter@me.com>
2015-02-06 07:09:45 -0500

    Switching to firefox

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,17 +1,18 @@
 var GulpSelenium = require('gulp-selenium');
-var gulpSelenium = GulpSelenium();
+var gulpSelenium = GulpSelenium(),
+    outputDir = process.env.CIRCLE_TEST_REPORTS || 'test-result';
 
 exports.config = {
   seleniumServerJar: gulpSelenium.path,
   chromeDriver: gulpSelenium.chromeDriverPath,
   //seleniumAddress: 'http://localhost:4444/wd/hub', // Using JAR instead of address
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'firefox'
   },
   onPrepare: function() {
     require('jasmine-reporters');
     jasmine.getEnv().addReporter(
-      new jasmine.JUnitXmlReporter('test-results', true, true)
+      new jasmine.JUnitXmlReporter(outputDir, true, true)
     );
   },
   specs: ['test/ui/**/*.spec.js']
