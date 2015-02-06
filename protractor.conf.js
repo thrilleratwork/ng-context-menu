556317dbe9bd0148f222c7c559b94277f4322feb
Ian Walter <iankwalter@me.com>
2015-02-06 07:16:21 -0500

    Trying to run selenium manually on circle

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,11 +3,11 @@ var gulpSelenium = GulpSelenium(),
     outputDir = process.env.CIRCLE_TEST_REPORTS || 'test-result';
 
 exports.config = {
-  seleniumServerJar: gulpSelenium.path,
-  chromeDriver: gulpSelenium.chromeDriverPath,
-  //seleniumAddress: 'http://localhost:4444/wd/hub', // Using JAR instead of address
+//  seleniumServerJar: gulpSelenium.path,
+//  chromeDriver: gulpSelenium.chromeDriverPath,
+  seleniumAddress: 'http://localhost:4444/wd/hub', // Using JAR instead of address
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'chrome'
   },
   onPrepare: function() {
     require('jasmine-reporters');
